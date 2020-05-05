f6bb39903aad3b4d21bef497679397663922b748
Praveen <pk99411@gmail.com>
2020-05-05 14:54:45 +0530

    day4 solution added

---
+++ Day5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character,Integer>map = new HashMap<>();
+        for(char i:s.toCharArray()){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        int index = 0;
+        for(char i:s.toCharArray()){
+            if(map.get(i) == 1){
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+}
