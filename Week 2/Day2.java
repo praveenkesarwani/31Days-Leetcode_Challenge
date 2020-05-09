4624b1ad9c38a3ecfe5e52a07837252c1b10f153
Praveen <pk99411@gmail.com>
2020-05-09 13:52:50 +0530

    Day2 solution added

--- Week 2/Day2.java
+++ Week 2/Day2.java
@@ -1,30 +1,28 @@
 // binary Search approach
 // Time Complexity: O(log(N))
 class Solution {
-    public boolean binary_search(int num){
-        if(num < 1){
+    public boolean binary_search(int num) {
+        if (num < 1) {
             return false;
-        }
-        else if(num == 1){
+        } else if (num == 1) {
             return true;
         }
         long left = 1;
         long right = num;
-        while(left <= right){
-            long mid = left + (right - left ) / 2;
-            if(mid * mid == num){
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            if (mid * mid == num) {
                 return true;
-            }
-            else if(mid * mid < num){
+            } else if (mid * mid < num) {
                 left = mid + 1;
-            }
-            else if(mid * mid > num){
+            } else if (mid * mid > num) {
                 right = mid - 1;
             }
         }
         return false;
     }
+
     public boolean isPerfectSquare(int num) {
-        return binary_search(num);
+        return isPerfectSquare(num);
     }
 }
