a4aa353e7f5696345bec2084d575b077247ed6cc
Praveen <pk99411@gmail.com>
2020-05-19 13:45:48 +0530

    Day5 Solution Added

---
+++ Week 3/Day5.java
@@ -0,0 +1,32 @@
+/*
+Time Complexity: O(Q), where Q is the number of calls to StockSpanner.next. 
+In total, there are Q pushes to the stack, and at most Q pops.
+Space Complexity: O(Q). 
+*/
+
+class StockSpanner {
+    Stack<Integer> prices, weights;
+
+    public StockSpanner() {
+        prices = new Stack();
+        weights = new Stack();
+    }
+
+    public int next(int price) {
+        int w = 1;
+        while (!prices.isEmpty() && prices.peek() <= price) {
+            prices.pop();
+            w += weights.pop();
+        }
+
+        prices.push(price);
+        weights.push(w);
+        return w;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
